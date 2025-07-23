9abfecfcda2e155afffa61604e7f3c5a94dc3c85
chasemc <chasemc@users.noreply.github.com>
2025-07-23 14:11:32 -0500

    add final project skeleton

--- tutorials/final_proj/main.py
+++ tutorials/final_proj/main.py
@@ -1,70 +1,53 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 import json
+import gzip 
 
 app = Flask(__name__)
 
-BGC0000001 = "data/mibig_data/BGC0000001.5.json"
-BGC0000002 = "data/mibig_data/BGC0000002.5.json"
 
-with open(BGC0000001, 'r') as f:
-    bgc1 = json.load(f)
-
-with open(BGC0000002, 'r') as f:
-    bgc2 = json.load(f)
-
-# Combine the data from both BGCs
-combined_data = {
-    "BGC0000001": bgc1,
-    "BGC0000002": bgc2
-}
-
-combined_data["BGC0000001"].keys()
-
-combined_data["BGC0000001"]["biosynthesis"]
-
-for i in combined_data["BGC0000001"]["biosynthesis"]:
-    print(i["name"])
+def load_bgc_json_data_into_memory():
+    BGC0000001 = "data/mibig_data/BGC0000001.5.json.gz"
+    BGC0000002 = "data/mibig_data/BGC0000002.5.json.gz"
+    with gzip.open(BGC0000001, "rt") as f:
+        bgc1 = json.load(f)
+    with gzip.open(BGC0000002, "rt") as f:
+        bgc2 = json.load(f)
+    # Combine the data from both BGCs
+    return {"BGC0000001": bgc1, "BGC0000002": bgc2}
 
 
+COMBINED_DATA = load_bgc_json_data_into_memory()
 
 
+@app.route("/bgc/quality/<bgc_id>", methods=["GET"])
+def get_quality(bgc_id):
+    """
+    Function to get the quality of a BGC.
+    """
+    # to test with curl:
+    # curl -X GET http://localhost:5000/bgc/quality/BGC0000001
+    try:
+        bgc_data = COMBINED_DATA[bgc_id]
+        quality = bgc_data.get("quality", "Quality data not available")
+        return jsonify({"bgc_id": bgc_id, "quality": quality}), 200
+    except KeyError:
+        return jsonify({"error": "BGC not found"}), 404
 
-# # GET - List all books
-# @app.route('/books', methods=['GET'])
-# def get_books():
-#     return jsonify(books)
 
-# # GET - Get one book by ID
-# @app.route('/books/<int:book_id>', methods=['GET'])
-# def get_book(book_id):
-#     book = next((b for b in books if b["id"] == book_id), None)
-#     if book:
-#         return jsonify(book)
-#     return jsonify({"error": "Book not found"}), 404
+# @app.route # need to fix this line
+def get_bgc_status():
+    pass  # and replace this "pass" with code
 
-# # POST - Add a new book
-# @app.route('/books', methods=['POST'])
-# def add_book():
-#     new_book = request.json
-#     books.append(new_book)
-#     return jsonify(new_book), 201
 
-# # PUT - Update a book
-# @app.route('/books/<int:book_id>', methods=['PUT'])
-# def update_book(book_id):
-#     book = next((b for b in books if b["id"] == book_id), None)
-#     if book:
-#         data = request.json
-#         book.update(data)
-#         return jsonify(book), 200
-#     return jsonify({"error": "Book not found"}), 404
+# @app.route # need to fix this line
+def get_bgc_biosynthetic_gene_names():
+    pass  # and replace this "pass" with code
 
-# # DELETE - Delete a book
-# @app.route('/books/<int:book_id>', methods=['DELETE'])
-# def delete_book(book_id):
-#     global books
-#     books = [b for b in books if b["id"] != book_id]
-#     return jsonify({"message": "Book deleted"}), 200
 
-if __name__ == '__main__':
-    print(bgc1)
+# @app.route # need to fix this line
+def get_bgc_compound_name_structure_and_activities():
+    pass  # and replace this "pass" with code
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
